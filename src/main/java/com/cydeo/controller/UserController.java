7c0acb01cc2f8a0185cf73c06d4451438a29472b
HP Laptop <katharina.slava@gmail.com>
2023-12-05 08:17:35 -0500

    ticketing-project-mvc

---
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    RoleService roleService;
+    UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/create")
+    public String createUser(Model model) {
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());//bring me all roles from dataBase
+        return "user/create";
+    }
+    @PostMapping("/create")
+    public  String insertUser(UserDTO user, Model model){
+      //  model.addAttribute("user", new UserDTO());
+
+       // model.addAttribute("roles", roleService.findAll());
+        userService.save(user);
+       // model.addAttribute("users", userService.findAll());//bring me all roles from dataBase
+
+
+        return "redirect:/user/create"; //user attribute, roles attribute, users
+    }
+}
