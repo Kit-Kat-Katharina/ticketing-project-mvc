39f1fdbcc7947a27b67bfd65af5253e7bcb534e6
HP Laptop <katharina.slava@gmail.com>
2023-12-07 08:58:01 -0500

    committing-updates

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
+
     ProjectService projectService;
     UserService userService;
-
 
     public ProjectController(ProjectService projectService, UserService userService) {
         this.projectService = projectService;
@@ -30,26 +33,59 @@ public class ProjectController {
         model.addAttribute("managers", userService.findManagers());
 
         return "/project/create";
+
     }
 
     @PostMapping("/create")
     public String insertProject(ProjectDTO project) {
-
         projectService.save(project);
-
         return "redirect:/project/create";
     }
 
-    @GetMapping("/delete/projectcode")
+    @GetMapping("/delete/{projectcode}")
     public String deleteProject(@PathVariable("projectcode") String projectcode) {
         projectService.deleteById(projectcode);
         return "redirect:/project/create";
-
     }
 
     @GetMapping("/complete/{projectcode}")
     public String completeProject(@PathVariable("projectcode") String projectcode) {
         projectService.complete(projectService.findById(projectcode));
         return "redirect:/project/create";
+    }
+
+    @GetMapping("/update/{projectcode}")
+    public String editProject(@PathVariable("projectcode") String projectcode, Model model) {
+
+        model.addAttribute("project", projectService.findById(projectcode));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("managers", userService.findManagers());
+
+        return "/project/update";
+
+    }
+
+    @PostMapping("/update")
+    public String updateProject(ProjectDTO project) {
+        projectService.update(project);
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/manager/project-status")
+    public String getProjectByManager(Model model){
+
+        UserDTO manager = userService.findById("john@cydeo.com");
+
+        List<ProjectDTO> projects = projectService.getCountedListOfProjectDTO(manager);
+
+        model.addAttribute("projects",projects);
+
+        return "/manager/project-status";
+    }
+
+    @GetMapping("/manager/complete/{projectCode}")
+    public String managerCompleteProject(@PathVariable("projectCode") String projectCode) {
+        projectService.complete(projectService.findById(projectCode));
+        return "redirect:/project/manager/project-status";
     }
 }
--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,21 +1,32 @@
 package com.cydeo.service.impl;
+
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
-public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> implements ProjectService {
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements ProjectService {
+
+    private final TaskService taskService;
+
+    public ProjectServiceImpl(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @Override
     public ProjectDTO save(ProjectDTO object) {
-        if(object.getStatus()==null){
-            object.setStatus(Status.OPEN);
+        if (object.getProjectStatus() == null) {
+            object.setProjectStatus(Status.OPEN);
         }
-        return super.save(object.getProjectCode(),object);
+        return super.save(object.getProjectCode(), object);
     }
 
     @Override
@@ -25,7 +36,12 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> im
 
     @Override
     public void update(ProjectDTO object) {
-        super.update(object.getProjectCode(),object);
+        ProjectDTO newProject = findById(object.getProjectCode());
+        if (object.getProjectStatus() == null) {
+            object.setProjectStatus(newProject.getProjectStatus());
+        }
+
+        super.update(object.getProjectCode(), object);
     }
 
     @Override
@@ -40,7 +56,38 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> im
 
     @Override
     public void complete(ProjectDTO project) {
-        project.setStatus(Status.COMPLETE);
+        project.setProjectStatus(Status.COMPLETE);
         super.save(project.getProjectCode(), project);
+    }
+
+    @Override
+    public List<ProjectDTO> findAllNonCompletedProjects() {
+        return findAll().stream().filter(project -> !project.getProjectStatus().equals(Status.COMPLETE)).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager) {
+
+        List<ProjectDTO> projectList =
+                findAll()
+                        .stream()
+                        .filter(project -> project.getAssignedManager().equals(manager))
+                        .map(project -> {
+
+                            List<TaskDTO> taskList = taskService.findTasksByManager(manager);
+
+                            int completeTaskCounts = (int) taskList.stream().filter(t -> t.getProject().equals(project) && t.getTaskStatus() == Status.COMPLETE).count();
+
+                            int unfinishedTaskCounts = (int) taskList.stream().filter(t -> t.getProject().equals(project) && t.getTaskStatus() != Status.COMPLETE).count();
+
+                            project.setCompleteTaskCounts(completeTaskCounts);
+                            project.setUnfinishedTaskCounts(unfinishedTaskCounts);
+
+                            return project;
+
+                        }).collect(Collectors.toList());
+
+
+        return projectList;
     }
 }
